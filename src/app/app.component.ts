73737cd2a249bd663725dcd04c0cc2a72649281a
kaizen23 <damianis2@poczta.onet.pl>
2016-12-20 20:53:14 +0100

    add dashboard component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
     //moduleId: module.id,
     selector: 'my-app',
     template: `
-        <my-heroes></my-heroes>
+       <nav>
+        <a routerLink="/dashboard">dashboard</a>
+        <a routerLink="/heroes">Heroes</a>
+       </nav>
+        <router-outlet></router-outlet>
         `
 })
 export class AppComponent { }
