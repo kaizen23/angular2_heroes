5845f93eb4ca3db0d49fa7d9b3f14229ca24f159
kaizen23 <damianis2@poczta.onet.pl>
2016-12-21 20:56:06 +0100

    add some new css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,15 @@ import { Component } from '@angular/core';
     //moduleId: module.id,
     selector: 'my-app',
     template: `
+    <h1>{{title}}</h1>
        <nav>
-        <a routerLink="/dashboard">dashboard</a>
-        <a routerLink="/heroes">Heroes</a>
+        <a routerLink="/dashboard" routerLinkActive="active">dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
        </nav>
         <router-outlet></router-outlet>
-        `
+        `,
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent { 
+     title = 'Tour of Heroes';
+}
